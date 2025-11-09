a32ab9d62a63c3f0326f0548f6980c08ecd2c67f
Jake <tebankai07@gmail.com>
2025-11-09 00:23:41 -0600

    Delete KnowledgeAreaRepository.java
    Tests

--- src/main/java/com/project/skillswap/logic/entity/KnowledgeArea/KnowledgeAreaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.project.skillswap.logic.entity.KnowledgeArea;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface KnowledgeAreaRepository extends JpaRepository<KnowledgeArea, Long> {
-    Optional<KnowledgeArea> findByNameIgnoreCase(String name);
-}
