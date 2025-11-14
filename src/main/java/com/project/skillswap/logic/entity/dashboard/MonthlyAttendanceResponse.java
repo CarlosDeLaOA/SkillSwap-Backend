2f3e2777bcb27ea79faaadd91ee9e680d2eb1416
josemarioarias12 <josemarioarias12@gmail.com>
2025-11-14 17:41:48 -0600

    Se actualiza el dashboard
    Se actualiza el dashboard

---
+++ src/main/java/com/project/skillswap/logic/entity/dashboard/MonthlyAttendanceResponse.java
@@ -0,0 +1,41 @@
+package com.project.skillswap.logic.entity.dashboard;
+
+public class MonthlyAttendanceResponse {
+
+    private String month;
+    private Integer presentes;
+    private Integer registrados;
+
+    public MonthlyAttendanceResponse() {
+    }
+
+    public MonthlyAttendanceResponse(String month, Integer presentes, Integer registrados) {
+        this.month = month;
+        this.presentes = presentes;
+        this.registrados = registrados;
+    }
+
+    public String getMonth() {
+        return month;
+    }
+
+    public void setMonth(String month) {
+        this.month = month;
+    }
+
+    public Integer getPresentes() {
+        return presentes;
+    }
+
+    public void setPresentes(Integer presentes) {
+        this.presentes = presentes;
+    }
+
+    public Integer getRegistrados() {
+        return registrados;
+    }
+
+    public void setRegistrados(Integer registrados) {
+        this.registrados = registrados;
+    }
+}
